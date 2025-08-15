b920d3fb865afa6e300cae7002f510f7869993c6
blurbeast <leumasre@gmail.com>
2025-08-15 07:49:58 +0100

    kitted

--- contracts/hello-world/src/lib.rs
+++ contracts/hello-world/src/lib.rs
@@ -1,13 +1,188 @@
 #![no_std]
-use soroban_sdk::{contract, contractimpl, vec, Env, String, Vec};
+use soroban_sdk::{contract, contracterror, contractimpl, contracttype, vec, Address, Env, String, Vec};
+
+mod test;
+
+
+static SECONDS_IN_TIME: u64 = 60;
+
+#[contracterror]
+#[derive(Debug, PartialEq,)]
+pub enum Sep0041Error {
+    InsufficientBalance = 1,
+
+}
+
+
+#[contracttype]
+#[derive(Debug, )]
+pub struct AllowanaceDetails {
+    pub amount: i128,
+    pub deadline: u64
+}
+
+#[contracttype]
+#[derive(Debug)]
+pub enum DataKey {
+    Balance(Address),
+    Name,
+    Symbol,
+    Decimal,
+    Admin,
+    TotalSupply,
+    Allowance(Address, Address)
+}
+
+
+#[contracttype]
+pub struct Approve {
+    from: Address,
+    spender: Address
+}
+
 
 #[contract]
-pub struct Contract;
+pub struct Sep0041;
+
+trait ISep0041 {
+    fn balance(env: &Env, id: Address) -> i128;
+    fn name(env: &Env) -> String;
+    fn decimals(env: &Env) -> u32;
+    fn symbol(env: Env) -> String;
+    fn mint(env: &Env, to: Address, amount: i128)  -> Result<bool, Sep0041Error> ;
+    fn allowance(env: &Env, from: Address, spender: Address) -> i128;
+    fn approve(env: &Env, from: Address, spender: Address, amount: i128, live_until_ledger: u32);
+    fn transfer(env: &Env, from: Address, to: Address, amount: i128);
+}
+
 #[contractimpl]
-impl Contract {
-    pub fn hello(env: Env, to: String) -> Vec<String> {
-        vec![&env, String::from_str(&env, "Hello"), to]
+impl Sep0041 {
+    pub fn __constructor(env: &Env, admin: Address, name: String, symbol: String, ) {
+        env.storage().instance().set(&DataKey::Admin, &admin);
+        env.storage().instance().set(&DataKey::Name, &name);
+        env.storage().instance().set(&DataKey::Symbol, &symbol);
+        env.storage().instance().set(&DataKey::Decimal, &18_u32);
     }
 }
 
-mod test;
+
+
+#[contractimpl]
+impl ISep0041 for Sep0041 {
+    // fn init() -> Result<>
+    fn balance(env: &Env, id: Address) -> i128 {
+        // get the balace from the storage
+        let balance_key: DataKey = DataKey::Balance(id);
+        env.storage().instance().get(&balance_key).unwrap_or(0_i128)
+    }
+
+    fn name(env: &Env) -> String {
+        env.storage().instance().get(&DataKey::Name).unwrap()
+    }
+    fn decimals(env: &Env) -> u32 {
+        env.storage().instance().get(&DataKey::Decimal).unwrap()
+    }
+    fn symbol(env: Env) -> String {
+        env.storage().instance().get(&DataKey::Symbol).unwrap()
+    }
+
+    fn mint(env: &Env, to: Address, amount: i128) -> Result<bool, Sep0041Error>{
+
+        // only admin
+        let admin: Address = env.storage().instance().get(&DataKey::Admin).unwrap();
+        // throw error next 
+        admin.require_auth();
+        // get the balance to
+
+        let to_balance = Self::balance(&env, to.clone());
+
+        // throw error if zero or less
+        if amount <= 0 {
+            return Err(Sep0041Error::InsufficientBalance);
+        }
+        let new_balance:i128 = to_balance + amount;   
+        // save the new balance
+        env.storage().instance().set(&DataKey::Balance(to.clone()),&(new_balance));
+        let total_supply: i128 = env.storage().instance().get(&DataKey::TotalSupply).unwrap();
+        env.storage().instance().set(&DataKey::TotalSupply, &(total_supply + amount));
+        Ok(true)
+    }
+
+    fn allowance(env: &Env, from: Address, spender: Address) -> i128 {
+        let tx_details: AllowanaceDetails = env.storage().instance().get(&DataKey::Allowance(from, spender)).unwrap();
+        tx_details.amount
+    }
+
+    fn approve(env: &Env, from: Address, spender: Address, amount: i128, live_until_ledger: u32) {
+
+        // authenticate the approver 'from'
+        from.require_auth();
+
+        // check the amount 
+        if amount <= 0 {
+            todo!()
+        }
+
+        // check the time 
+        if env.ledger().timestamp() >= (live_until_ledger as u64 * SECONDS_IN_TIME) {
+            todo!()
+        }
+
+        // check if there was a previous one before , if yes , remove
+        if env.storage().instance().has(&DataKey::Allowance(from.clone(), spender.clone())) {
+            env.storage().instance().remove(&DataKey::Allowance(from.clone(), spender.clone()));   
+        }
+
+        //now create the details and save 
+        let tx_details: AllowanaceDetails = AllowanaceDetails {
+            amount: amount,
+            deadline: (env.ledger().timestamp() + (live_until_ledger as u64 * SECONDS_IN_TIME))
+        };
+
+        env.storage().instance().set(&DataKey::Allowance(from.clone(), spender.clone()), &tx_details);
+
+    }
+
+    fn transfer(env: &Env, from: Address, to: Address, amount: i128) {
+
+        from.require_auth();
+
+        if amount <= 0 {
+            todo!()
+        }
+
+        Self::_transfer(env, from, to, amount);
+        // // check from balance 
+        // let from_balance: i128 = env.storage().instance().get(&DataKey::Balance(from.clone())).unwrap();
+
+        // assert!(from_balance >= amount, "insufficient");
+
+        // // 
+        // let to_balance: i128 = env.storage().instance().get(&DataKey::Balance(to.clone())).unwrap();
+
+        // let from_new_balance: i128 = from_balance - amount;
+        // let to_new_balance: i128 = to_balance + amount;
+
+        // // update the state
+        // env.storage().instance().set(&DataKey::Balance(from.clone()), &from_new_balance);
+        // env.storage().instance().set(&DataKey::Balance(to.clone()), &to_new_balance);
+    }
+}
+
+impl Sep0041 {
+    
+
+    fn _transfer(env: &Env, from: Address, to: Address, amount: i128) {
+
+         let from_balance: i128 = env.storage().instance().get(&DataKey::Balance(from.clone())).unwrap();
+
+        // to balance
+        let to_balance: i128 = env.storage().instance().get(&DataKey::Balance(to.clone())).unwrap();
+
+        let from_new_balance: i128 = from_balance - amount;
+        let to_new_balance: i128 = to_balance + amount;
+
+        env.storage().instance().set(&DataKey::Balance(from.clone()), &from_new_balance);
+        env.storage().instance().set(&DataKey::Balance(to.clone()), &to_new_balance);
+    }
+}
--- contracts/hello-world/src/test.rs
+++ contracts/hello-world/src/test.rs
@@ -1,21 +1,59 @@
-#![cfg(test)]
+// #![cfg(test)]
 
-use super::*;
-use soroban_sdk::{vec, Env, String};
+// use super::*;
+// use soroban_sdk::{vec, Env, String};
 
-#[test]
-fn test() {
-    let env = Env::default();
-    let contract_id = env.register(Contract, ());
-    let client = ContractClient::new(&env, &contract_id);
+// #[test]
+// fn test() {
+//     let env = Env::default();
+//     let contract_id = env.register(Contract, ());
+//     let client = ContractClient::new(&env, &contract_id);
 
-    let words = client.hello(&String::from_str(&env, "Dev"));
-    assert_eq!(
-        words,
-        vec![
-            &env,
-            String::from_str(&env, "Hello"),
-            String::from_str(&env, "Dev"),
-        ]
-    );
+//     let words = client.hello(&String::from_str(&env, "Dev"));
+//     assert_eq!(
+//         words,
+//         vec![
+//             &env,
+//             String::from_str(&env, "Hello"),
+//             String::from_str(&env, "Dev"),
+//         ]
+//     );
+// }
+
+
+#[cfg(test)]
+mod test {
+    use core::ops::Add;
+
+    use soroban_sdk::{testutils::Address as _, Address, log, Env, String};
+
+    fn generate_addresses(env: &Env) -> (Address, Address, Address) {
+        let first_address: Address = Address::generate(env);
+        let second_addess: Address = Address::generate(env);
+        let third_address: Address = Address::generate(env);
+
+        (first_address, second_addess, third_address)
+    }
+
+    use crate::{Sep0041, Sep0041Client};
+    fn setup() -> (Env, Sep0041Client<'static>, Address) {
+        let env: Env = Env::default();
+
+        let (admin, _, _) = generate_addresses(&env);
+        let contract_id = env.register(Sep0041, (admin, String::from_str(&env, "loaded"), String::from_str(&env, "lsd",)));
+        let contract_client = Sep0041Client::new(&env, &contract_id);
+        env.mock_all_auths();
+        (env, contract_client, contract_id)
+    }
+    #[test]
+    fn test_name_and_symbol() {
+        let (env, contract_instance, _) = setup();
+        let contract_name = contract_instance.name();
+        let constract_symbol: String = contract_instance.symbol();
+
+        assert_eq!(contract_name, String::from_str(&env, "loaded"));
+        assert_eq!(constract_symbol, String::from_str(&env, "lsd"))
+
+        
+    }
 }
